0ae31f57e3d7bd4be1e0b1efa8888f3c94554512
Sam White (whites27) <whites27@coventry.ac.uk>
2023-04-12 14:08:45 +0100

    Pause menu button function

--- Assets/Sam/Scripts/PauseController.cs
+++ Assets/Sam/Scripts/PauseController.cs
@@ -1,9 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Stuart;
+using UnityEngine.SceneManagement;
 
 public class PauseController : MonoBehaviour
 {
-    
+    public Canvas PauseUI;
 
+    public void Awake()
+    {
+        Play();
+    }
+
+    public void Start()
+    {
+        GameController.Instance.OnPauseChanged += PausedChanged;
+    }
+
+    public void PausedChanged(bool value)
+    {
+        Debug.Log(value);
+
+        if (value)
+        {
+            Pause();
+        }
+        else
+        {
+            Play();
+        }
+    }
+
+    public void Play()
+    {
+        PauseUI.enabled = false;
+
+    }
+
+    public void Pause()
+    {
+        PauseUI.enabled = true;
+    }
+
+    public void QuitButton()
+    {
+        Debug.Log("Game has Exited");
+        Application.Quit();
+    }
+
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void Continue()
+    {
+        
+    }
 }
