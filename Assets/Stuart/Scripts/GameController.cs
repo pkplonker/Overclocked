26886fb3d73eb1e6e3262ee8dd0c67a5559cf1c1
Stuart Heath <heaths8@uni.coventry.ac.uk>
2023-04-12 11:45:31 +0100

    game timer and game over actions

--- Assets/Stuart/Scripts/GameController.cs
+++ Assets/Stuart/Scripts/GameController.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 
 namespace Stuart
 {
+    //todo change to state machine?
     public class GameController : MonoBehaviour
     {
         public static GameController Instance;
         [SerializeField] private float gameStartInitialDelay = 1f;
         [SerializeField] private float gameStartCountdown = 3f;
+        [SerializeField] private float gameTimeSeconds = 180f;
         private List<CharacterMovement> players;
         [SerializeField] private KeyCode pauseKey = KeyCode.Escape;
         private bool isPaused;
-        public event Action<bool> OnPauseChanged; 
+        public event Action<bool> OnPauseChanged;
+        public event Action<float> OnGameStartTimerTick;
+        public event Action<float,float> OnGameTick;
+        public event Action OnGameOver;
+        public event Action OnGameStart;
         private void Awake()
         {
             if (Instance == null) Instance = this;
@@ -22,9 +28,6 @@ namespace Stuart
         }
 
         private void Start()=>StartCoroutine(GameStartCor());
-        public event Action<float> OnGameStartTimerTick;
-        public event Action OnGameStart;
-
         private IEnumerator GameStartCor()
         {
             var countdown = 0f;
@@ -51,17 +54,46 @@ namespace Stuart
                 PauseGameToggle();
         }
 
-        private void PauseGameToggle()
+        private void PauseGameToggle()=>SetPause(!isPaused);
+        
+
+        public void SetPause(bool state)
         {
-            isPaused = !isPaused;
-            SetPlayerMovement(false);
+            isPaused = state;
+            SetPlayerMovement(!isPaused);
             OnPauseChanged?.Invoke(isPaused);
         }
 
         private void GameStart()
         {
             SetPlayerMovement(true);
+#if UNITY_EDITOR
+            Debug.Log("Game Start");
+#endif
             OnGameStart?.Invoke();
+            StartCoroutine(GameTick());
+        }
+
+        private IEnumerator GameTick()
+        {
+            var countdown = 0f;
+            while (countdown < gameTimeSeconds)
+            {
+                countdown += Time.deltaTime;
+                OnGameTick?.Invoke(countdown,gameTimeSeconds);
+                yield return null;
+            }
+
+            SetGameOver();
+        }
+
+        private void SetGameOver()
+        {
+#if UNITY_EDITOR
+            Debug.Log("GameOver");
+#endif
+            SetPlayerMovement(false);
+            OnGameOver?.Invoke();
         }
 
         private void SetPlayerMovement(bool canMove)
