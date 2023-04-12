75e7d5e564988d6982e8609029d9251d9c3486f1
Stuart Heath <heaths8@uni.coventry.ac.uk>
2023-04-12 16:28:22 +0100

    Bin

--- Assets/Stuart/Scripts/Item/Inventory.cs
+++ Assets/Stuart/Scripts/Item/Inventory.cs
@@ -39,8 +39,9 @@ namespace Stuart
 
         private void DropItem(ItemBaseSO item)
         {
+            if (item == null) return;
             var go = Instantiate(item.GetPrefab(), new Vector3(transform.position.x, groundOffset, transform.position.z),
-                currentItem.GetPrefab().transform.rotation);
+                item.GetPrefab().transform.rotation);
             go.GetComponent<Item>().itemSO = currentItem;
             go.AddComponent<ItemPickup>();
         }
