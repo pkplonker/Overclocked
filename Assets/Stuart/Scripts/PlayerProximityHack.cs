c87e48f74bea92881cd9be71a790d45a1fb89b45
stuart <pkplonker@gmail.com>
2023-04-12 22:59:26 +0100

    player proximity hack

---
+++ Assets/Stuart/Scripts/PlayerProximityHack.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+//hack class
+public class PlayerProximityHack : MonoBehaviour
+{
+    private Canvas canvas;
+    private float radius = 1.5f;
+    private void Awake()
+    {
+        canvas = GetComponent<Canvas>();
+    }
+
+    private void Update()
+    {
+	    canvas.enabled = false;
+     var res=   Physics.OverlapSphere(transform.position, radius).ToList();
+     foreach (var r in res)
+     {
+	     if (r.CompareTag("Player")) canvas.enabled = true;
+     }
+    }
+}
