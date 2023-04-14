ebffd09f888a61bb3fe938442ec80906cd314e13
Stuart Heath <heaths8@uni.coventry.ac.uk>
2023-04-14 13:42:17 +0100

    game over changes

--- Assets/Sam/Scripts/GameOver.cs
+++ Assets/Sam/Scripts/GameOver.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using Stuart;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -12,23 +9,40 @@ public class GameOver : MonoBehaviour
 
     private void Start()
     {
-        GameController.Instance.OnGameOver += (_) => loseScreen.SetActive(true);
-        JobFactory.OnWin += () => winScreen.SetActive(true);
+        GameController.Instance.OnGameOver += Lose;
+        JobFactory.OnWin += Win;
         loseScreen.SetActive(false);
         winScreen.SetActive(false);
-
     }
+
+    private void OnDisable()
+    {
+        GameController.Instance.OnGameOver -= Lose;
+        JobFactory.OnWin -= Win;
+    }
+
+
+    private void Win()
+    {
+        winScreen.SetActive(true);
+    }
+
+    private void Lose(bool r)
+    {
+        loseScreen.SetActive(true);
+    }
+
 
     public void ReturnToMainMenu()
     {
         SceneManager.LoadScene("Menu");
     }
-     
+
     public void NextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-    
+
     public void RetryLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
